9d169a4640d5f5823b05926a5d93176024a4bc77
therelyona <soboleva.alyonka@gmail.com>
2025-03-11 19:07:51 +0400

    add Logo component with styles and image

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,26 @@
+import './Logo.scss';
+import classNames from 'classnames';
+
+const Logo = ({ loading = 'lazy', className}) => {
+  const title = 'Home';
+
+  return (
+    <a
+      className={classNames(className, 'logo')}
+      href="/"
+      title={title}
+      aria-label={title}
+    >
+      <img
+        src="/logo.svg"
+        alt=""
+        className="logo__image"
+        width={199}
+        height={60}
+        loading={loading}
+      />
+    </a>
+  );
+};
+
+export default Logo;
