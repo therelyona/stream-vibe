f3cb21ec845de9dcb5aa91af128321422bca3d24
therelyona <soboleva.alyonka@gmail.com>
2025-04-17 16:46:18 +0400

    add seasonInfo.js with show data

---
+++ src/components/Seasons/seasonInfo.js
@@ -0,0 +1,88 @@
+export const description = 'When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and one strange little girl.'
+
+export const castItems = [
+  { imgSrc: '/src/assets/images/season-people/1.jpg', imgAlt: 'Winona Ryder' },
+  { imgSrc: '/src/assets/images/season-people/2.jpg', imgAlt: 'David Harbour' },
+  { imgSrc: '/src/assets/images/season-people/3.jpg', imgAlt: 'Millie Bobby Brown' },
+  { imgSrc: '/src/assets/images/season-people/4.jpg', imgAlt: 'Finn Wolfhard' },
+  { imgSrc: '/src/assets/images/season-people/5.jpg', imgAlt: 'Gaten Matarazzo' },
+  { imgSrc: '/src/assets/images/season-people/6.jpg', imgAlt: 'Caleb McLaughlin' },
+  { imgSrc: '/src/assets/images/season-people/7.jpg', imgAlt: 'Noah Schnapp' },
+  { imgSrc: '/src/assets/images/season-people/8.jpg', imgAlt: 'Sadie Sink' },
+  { imgSrc: '/src/assets/images/season-people/1.jpg', imgAlt: 'Winona Ryder' },
+  { imgSrc: '/src/assets/images/season-people/2.jpg', imgAlt: 'David Harbour' },
+  { imgSrc: '/src/assets/images/season-people/3.jpg', imgAlt: 'Millie Bobby Brown' },
+  { imgSrc: '/src/assets/images/season-people/4.jpg', imgAlt: 'Finn Wolfhard' },
+  { imgSrc: '/src/assets/images/season-people/5.jpg', imgAlt: 'Gaten Matarazzo' },
+  { imgSrc: '/src/assets/images/season-people/6.jpg', imgAlt: 'Caleb McLaughlin' },
+  { imgSrc: '/src/assets/images/season-people/7.jpg', imgAlt: 'Noah Schnapp' },
+  { imgSrc: '/src/assets/images/season-people/8.jpg', imgAlt: 'Sadie Sink' },
+]
+
+export const reviewItems = [
+  {
+    name: 'Aniket Roy',
+    subtitle: 'From India',
+    description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+    ratingValue: 4.5,
+  },
+  {
+    name: 'Swaraj',
+    subtitle: 'From India',
+    description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+    ratingValue: 5,
+  },
+  {
+    name: 'Aniket Roy',
+    subtitle: 'From India',
+    description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+    ratingValue: 4.5,
+  },
+  {
+    name: 'Swaraj',
+    subtitle: 'From India',
+    description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+    ratingValue: 5,
+  },
+  {
+    name: 'Aniket Roy',
+    subtitle: 'From India',
+    description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+    ratingValue: 4.5,
+  },
+  {
+    name: 'Swaraj',
+    subtitle: 'From India',
+    description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+    ratingValue: 5,
+  },
+  {
+    name: 'Aniket Roy',
+    subtitle: 'From India',
+    description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+    ratingValue: 4.5,
+  },
+  {
+    name: 'Swaraj',
+    subtitle: 'From India',
+    description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+    ratingValue: 5,
+  },
+]
+
+export const gernes = ['Sci-Fi TV', 'Teen TV Shows', 'US TV Shows']
+
+export const director = {
+  name: 'The Duffer Brothers',
+  subtitle: 'From USA',
+  imgSrc: '/src/assets/images/season-people/director.jpg',
+  imgAlt: 'The Duffer Brothers'
+}
+
+export const music = {
+  name: 'Kyle Dixon',
+  subtitle: 'From USA',
+  imgSrc: '/src/assets/images/season-people/music.jpg',
+  imgAlt: 'Kyle Dixon'
+}
+
