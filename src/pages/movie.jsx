d9458b2c6dfcefdc18605ccee5693a90409855aa
therelyona <soboleva.alyonka@gmail.com>
2025-04-17 16:48:50 +0400

    import and pass movie data as props to MovieDetails section

--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -1,5 +1,11 @@
 import MovieBanner from '@/sections/MovieBanner'
 import MovieDetails from '@/sections/MovieDetails'
+import { description } from '@/sections/MovieDetails/movieInfo'
+import { castItems } from '@/sections/MovieDetails/movieInfo'
+import { gernes } from '@/sections/MovieDetails/movieInfo'
+import { director } from '@/sections/MovieDetails/movieInfo'
+import { music } from '@/sections/MovieDetails/movieInfo'
+import { reviewItems } from '@/sections/MovieDetails/movieInfo'
 
 export const metadata = {
   title: 'Movie - Kantara',
@@ -9,7 +15,14 @@ export default function () {
   return (
     <>
       <MovieBanner />
-      <MovieDetails />
+      <MovieDetails
+        description={description}
+        cast={castItems}
+        reviewItems={reviewItems}
+        gernes={gernes}
+        director={director}
+        music={music}
+      />
     </>
   )
 }
