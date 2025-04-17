97c6d3876239080fffc1708298234ccf7f61d836
therelyona <soboleva.alyonka@gmail.com>
2025-04-17 16:49:34 +0400

    import and pass movie data as props to MovieDetails section

--- src/pages/show.jsx
+++ src/pages/show.jsx
@@ -1,6 +1,12 @@
 import ShowBanner from '@/sections/ShowBanner'
 import MovieDetails from '@/sections/MovieDetails'
 import Seasons from '@/components/Seasons'
+import { description } from '@/components/Seasons/seasonInfo'
+import { castItems } from '@/components/Seasons/seasonInfo'
+import { reviewItems } from '@/components/Seasons/seasonInfo'
+import { gernes } from '@/components/Seasons/seasonInfo'
+import { director } from '@/components/Seasons/seasonInfo'
+import { music } from '@/components/Seasons/seasonInfo'
 
 export const metadata = {
   title: 'Show - Stranger Things',
@@ -12,6 +18,12 @@ export default function () {
       <ShowBanner />
       <MovieDetails
         seasons={<Seasons />}
+        description={description}
+        cast={castItems}
+        reviewItems={reviewItems}
+        gernes={gernes}
+        director={director}
+        music={music}
       />
     </>
   )
