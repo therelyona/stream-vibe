a608b427ccfc43a6e70313e515801b77a4aaa30e
therelyona <soboleva.alyonka@gmail.com>
2025-04-17 16:47:21 +0400

    extract movie data into movieInfo.js for reuse in MovieDetails section

---
+++ src/sections/MovieDetails/movieInfo.js
@@ -0,0 +1,87 @@
+export const description = 'A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands.'
+
+export const castItems = [
+  { imgSrc: '/src/assets/images/people/1.jpg', imgAlt: 'Kaadubettu Shiva' },
+  { imgSrc: '/src/assets/images/people/2.jpg', imgAlt: 'Murali' },
+  { imgSrc: '/src/assets/images/people/3.jpg', imgAlt: 'Devendra Suttooru' },
+  { imgSrc: '/src/assets/images/people/4.jpg', imgAlt: 'Leela' },
+  { imgSrc: '/src/assets/images/people/5.jpg', imgAlt: 'Manasi Sudhir' },
+  { imgSrc: '/src/assets/images/people/6.jpg', imgAlt: 'Pramod Shetty' },
+  { imgSrc: '/src/assets/images/people/7.jpg', imgAlt: 'Ranjan Saju' },
+  { imgSrc: '/src/assets/images/people/8.jpg', imgAlt: 'Garnall Abbu' },
+  { imgSrc: '/src/assets/images/people/1.jpg', imgAlt: 'Kaadubettu Shiva' },
+  { imgSrc: '/src/assets/images/people/2.jpg', imgAlt: 'Murali' },
+  { imgSrc: '/src/assets/images/people/3.jpg', imgAlt: 'Devendra Suttooru' },
+  { imgSrc: '/src/assets/images/people/4.jpg', imgAlt: 'Leela' },
+  { imgSrc: '/src/assets/images/people/5.jpg', imgAlt: 'Manasi Sudhir' },
+  { imgSrc: '/src/assets/images/people/6.jpg', imgAlt: 'Pramod Shetty' },
+  { imgSrc: '/src/assets/images/people/7.jpg', imgAlt: 'Ranjan Saju' },
+  { imgSrc: '/src/assets/images/people/8.jpg', imgAlt: 'Garnall Abbu' },
+]
+
+export const reviewItems = [
+  {
+    name: 'Aniket Roy',
+    subtitle: 'From India',
+    description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+    ratingValue: 4.5,
+  },
+  {
+    name: 'Swaraj',
+    subtitle: 'From India',
+    description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+    ratingValue: 5,
+  },
+  {
+    name: 'Aniket Roy',
+    subtitle: 'From India',
+    description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+    ratingValue: 4.5,
+  },
+  {
+    name: 'Swaraj',
+    subtitle: 'From India',
+    description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+    ratingValue: 5,
+  },
+  {
+    name: 'Aniket Roy',
+    subtitle: 'From India',
+    description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+    ratingValue: 4.5,
+  },
+  {
+    name: 'Swaraj',
+    subtitle: 'From India',
+    description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+    ratingValue: 5,
+  },
+  {
+    name: 'Aniket Roy',
+    subtitle: 'From India',
+    description: 'This movie was recommended to me by a very dear friend who went for the movie by herself. I went to the cinemas to watch but had a houseful board so couldn’t watch it.',
+    ratingValue: 4.5,
+  },
+  {
+    name: 'Swaraj',
+    subtitle: 'From India',
+    description: 'A restless king promises his lands to the local tribals in exchange of a stone (Panjurli, a deity of Keradi Village) wherein he finds solace and peace of mind.',
+    ratingValue: 5,
+  },
+]
+
+export const gernes = ['Action', 'Adventure']
+
+export const director = {
+  name: 'Rishab Shetty',
+  subtitle: 'From India',
+  imgSrc: '/src/assets/images/people/director.jpg',
+  imgAlt: 'Rishab Shetty'
+}
+
+export const music = {
+  name: 'B. Ajaneesh Loknath',
+  subtitle: 'From India',
+  imgSrc: '/src/assets/images/people/music.jpg',
+  imgAlt: 'B. Ajaneesh Loknath'
+}
